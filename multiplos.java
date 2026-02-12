4b357b36bc3acf75887dba9550af3f9a4c7b4506
joel-boop12 <joeldelalamo06@gmail.com>
2026-02-12 12:22:16 +0100

    V4

--- multiplos.java
+++ multiplos.java
@@ -5,7 +5,7 @@ gfhfghgh
             System.out.println("Multiplo de 7: "+i);
         }
         if (i%49==0){
-            System.out.println("MUltiplo de 49: "+i); 
+            System.out.println("Multiplo de 49: "+i); 
         }
         else if (i%100==0){ 
             System.out.println("Multiplo de 100: "+i); 
