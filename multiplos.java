8606e5c0c67c9045661a5c42276abfc5bcb72f8e
joel-boop12 <joeldelalamo06@gmail.com>
2026-02-12 12:19:07 +0100

    V3

--- multiplos.java
+++ multiplos.java
@@ -1,6 +1,9 @@
 public static void multiplos(String[] args) {
-
-    for(int=0;i<=numeroMax;i++){
+gfhfghgh
+    for (int i=0; i<=numeroMax;i++){
+        if(i%7==0){
+            System.out.println("Multiplo de 7: "+i);
+        }
         if (i%49==0){
             System.out.println("Multiplo de 49: "+i); 
         }
@@ -10,3 +13,5 @@ public static void multiplos(String[] args) {
     }
     
 }
+gdfgdfgfdzz
+gjghjgjgdjgyjd
