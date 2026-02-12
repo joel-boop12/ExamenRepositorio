4249f63498b2e23efb2aea02a7effdea08fbc513
joel-boop12 <joeldelalamo06@gmail.com>
2026-02-12 12:06:24 +0100

    RN

---
+++ multiplos.java
@@ -0,0 +1,12 @@
+public static void multiplos(String[] args) {
+
+    for(int=0;i<=numeroMax;i++){
+        if (i%49==0){
+            System.out.println("Multiplo de 49: "+i); 
+        }
+        else if (i%100==0){ 
+            System.out.println("Multiplo de 100: "+i); 
+        }
+    }
+    
+}
